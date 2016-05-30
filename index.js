97fc977156f2dc88dfc8dd45e944ee1807050775
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-05-30 20:44:12 +0200

    lib(): add package

---
+++ index.js
@@ -0,0 +1,21 @@
+// ------------------------------------
+// #POSTCSS - LOAD PLUGINS
+// ------------------------------------
+
+'use strict'
+
+const config = require('cosmiconfig')
+
+const loadPlugins = require('./lib/loadPlugins')
+const loadOptions = require('./lib/loadOptions')
+
+module.exports = function (options) {
+  return config('postcss')
+    .catch((error) => console.log(error))
+    .then((result) => {
+      return {
+        plugins: loadPlugins(result.config, options),
+        options: loadOptions(result.config, options)
+      }
+    })
+}
