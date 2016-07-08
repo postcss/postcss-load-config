0046cab161ba146402444be7d411d820a8f8e696
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-07-08 09:41:16 +0200

    lib(index): add ES5 compatibility due node v0.12 support

--- index.js
+++ index.js
@@ -4,15 +4,17 @@
 
 'use strict'
 
-const config = require('cosmiconfig')
+var config = require('cosmiconfig')
 
-const loadOptions = require('postcss-load-options/lib/loadOptions.js')
-const loadPlugins = require('postcss-load-plugins/lib/loadPlugins.js')
+var loadOptions = require('postcss-load-options/lib/loadOptions.js')
+var loadPlugins = require('postcss-load-plugins/lib/loadPlugins.js')
 
 module.exports = function loadConfig (options) {
   return config('postcss')
-    .catch((error) => console.log(error))
-    .then((result) => {
+    .catch(function (error) {
+      console.log(error)
+    })
+    .then(function (result) {
       return {
         plugins: loadPlugins(result.config, options),
         options: loadOptions(result.config, options)
