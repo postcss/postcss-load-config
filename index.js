a6c32fd1e9ddf26a506691fc5e1861221a70a8e8
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-01-11 12:20:31 +0100

    feat(index): config.file, improve error handling

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // ------------------------------------
-// #POSTCSS - LOAD CONFIG - INDEX
+// # POSTCSS - LOAD CONFIG - INDEX
 // ------------------------------------
 
 'use strict'
+
+var resolve = require('path').resolve
 
 var config = require('cosmiconfig')
 var assign = require('object-assign')
@@ -11,11 +13,13 @@ var loadOptions = require('postcss-load-options/lib/options.js')
 var loadPlugins = require('postcss-load-plugins/lib/plugins.js')
 
 /**
+ * Autoload Config for PostCSS
+ *
  * @author Michael Ciniawsky (@michael-ciniawsky) <michael.ciniawsky@gmail.com>
- * @description Autoload Config for PostCSS
+ * @license MIT
  *
  * @module postcss-load-config
- * @version 1.0.0
+ * @version 1.1.0
  *
  * @requires comsiconfig
  * @requires object-assign
@@ -28,47 +32,38 @@ var loadPlugins = require('postcss-load-plugins/lib/plugins.js')
  * @param  {String} path Config Directory
  * @param  {Object} options Config Options
  *
- * @return {Promise} config  PostCSS Plugins, PostCSS Options
+ * @return {Promise} config PostCSS Config
  */
 module.exports = function postcssrc (ctx, path, options) {
-  var defaults = { cwd: process.cwd(), env: process.env.NODE_ENV }
+  ctx = assign({ cwd: process.cwd(), env: process.env.NODE_ENV }, ctx)
 
-  ctx = assign(defaults, ctx)
-  path = path || process.cwd()
-  options = options || {}
+  path = path ? resolve(path) : process.cwd()
 
-  if (ctx.env === undefined) {
-    process.env.NODE_ENV = 'development'
-  }
+  options = assign({}, options)
+
+  if (!ctx.env) process.env.NODE_ENV = 'development'
+
+  var file
 
   return config('postcss', options)
     .load(path)
     .then(function (result) {
-      if (!result) {
-        console.log(
-          'PostCSS Config could not be loaded. Please check your PostCSS Config.'
-        )
-      }
+      if (!result) throw Error('No PostCSS Config found in: ' + path)
+
+      file = result ? result.filepath : ''
 
       return result ? result.config : {}
     })
     .then(function (config) {
-      if (typeof config === 'function') {
-        config = config(ctx)
-      } else {
-        config = assign(config, ctx)
-      }
+      if (typeof config === 'function') config = config(ctx)
+      else config = assign(config, ctx)
 
-      if (!config.plugins) {
-        config.plugins = []
-      }
+      if (!config.plugins) config.plugins = []
 
       return {
         plugins: loadPlugins(config),
-        options: loadOptions(config)
+        options: loadOptions(config),
+        file: file
       }
-    })
-    .catch(function (err) {
-      console.log(err)
     })
 }
