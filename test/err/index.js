2ef00065fbf6d0733a6efe905cfae72e4b0689ff
Duncan Beevers <duncan@dweebd.com>
2017-06-21 07:11:24 -0700

    test(err): account for different development paths (#90)

--- test/err/index.js
+++ test/err/index.js
@@ -9,7 +9,9 @@ var test = require('ava')
 var postcssrc = require('../..')
 
 test('No Config - {Error}', function (t) {
+  t.plan(1);
+
   return postcssrc({}, 'test').catch(function (err) {
-    t.is(err.message, 'No PostCSS Config found in: /home/travis/build/michael-ciniawsky/postcss-load-config/test')
+    t.regex(err.message, /^No PostCSS Config found in: .*\/postcss-load-config\/test$/)
   })
 })
