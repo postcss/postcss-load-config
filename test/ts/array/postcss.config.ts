bf62e05f8488a454845571c660beed22f8105024
Gareth Jones <jones258@gmail.com>
2021-05-28 14:50:13 +1200

    test: add fixtures

---
+++ test/ts/array/postcss.config.ts
@@ -0,0 +1,18 @@
+import postcssImport from 'postcss-import';
+import postcssNested from 'postcss-nested';
+import cssnano from 'cssnano';
+
+const config = ctx => ({
+  parser: ctx.parser ? 'sugarss' : false,
+  syntax: ctx.syntax ? 'sugarss' : false,
+  map: ctx.map ? 'inline' : false,
+  from: './test/ts/array/fixtures/index.css',
+  to: './test/ts/array/expect/index.css',
+  plugins: [
+    postcssImport(),
+    postcssNested(),
+    ctx.env === 'production' ? cssnano() : false
+  ]
+})
+
+export default config;
---
+++ test/ts/object/postcss.config.ts
@@ -0,0 +1,14 @@
+const config = ctx => ({
+  parser: ctx.parser ? 'sugarss' : false,
+  syntax: ctx.syntax ? 'sugarss' : false,
+  map: ctx.map ? 'inline' : false,
+  from: './test/ts/object/fixtures/index.css',
+  to: './test/ts/object/expect/index.css',
+  plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    cssnano: ctx.env === 'production' ? {} : false
+  }
+})
+
+export default config;
